b2d8a8354258d1e048cbe623e3f3ccbefc70bfd9
Jeremy Greer <jex.grizzle@gmail.com>
2019-06-09 00:46:18 -0700

    Test initial mock values

--- src/basic-mocks.test.js
+++ src/basic-mocks.test.js
@@ -4,17 +4,14 @@ import * as proxySpec from './proxy-ImportSpecifier'
 
 jest.mock('./config')
 
-xdescribe.each([
+describe.each([
   [proxyDefault, 'ImportDefaultSpecifier'],
   [proxyNamespace, 'ImportNamespaceSpecifier'],
   [proxySpec, 'ImportSpecifier'],
 ])('basic mocks from __mocks__/', (proxy, name) => {
 
   it(`${name} should expose some stuff from config`, () => {
-    // By default, I want these automocks to look like the values from the
-    // original.  Let's deal with that later, though.
-    expect(proxy.getName()).toBe('Diz')
-    expect(proxy.getAge()).toBe(99)
-    expect(proxy.getColor()).toBe('blue')
+    expect(proxy.getName()).toBe('John')
+    expect(proxy.getColor()).toBe('red')
   })
 })
